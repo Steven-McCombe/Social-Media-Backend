51c8a74e059f19f4ebb905ab58068d8b8b537183
Steven-McCombe <smccombe93@gmail.com>
2023-02-20 20:37:34 -0500

    added controllers updated error in usersRoute

--- models/index.js
+++ models/index.js
@@ -1,30 +1,4 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const User = require('./user');
+const Thought = require('./thought');
 
-const ReactionSchema = new Schema({
-  reactionId: {
-    type: mongoose.Types.ObjectId,
-    default: mongoose.Types.ObjectId
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-ReactionSchema.set('toJSON', {
-  getters: true
-});
-
-const Reaction = mongoose.model('Reaction', ReactionSchema);
-
-module.exports = Reaction;
+module.exports = { User, Thought };
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
+// Suppress Mongoose deprecation warning for `strictQuery`
+mongoose.set('strictQuery', false);
+
+// Establish connection with MongoDB
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
+
+// Use routes
+app.use(routes);
