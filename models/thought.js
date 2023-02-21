3f371a625eead4da6520f898bf927ceef329ef54
Steven-McCombe <smccombe93@gmail.com>
2023-02-20 22:05:36 -0500

    fixed bug in reactions

--- models/thought.js
+++ models/thought.js
@@ -3,10 +3,6 @@ const { Schema } = mongoose;
 
 const ReactionSchema = new Schema(
   {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
     reactionBody: {
       type: String,
       required: true,
